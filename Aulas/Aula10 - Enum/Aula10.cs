d2741fc95ccf2a3f0efaf864d669cd3b3a05ab51
Mateus Fernando <matloko1000@gmail.com>
2023-04-16 17:26:40 -0300

    Teste sobre Enums + Enums

--- Aulas/Aula10 - Enum/Aula10.cs
+++ Aulas/Aula10 - Enum/Aula10.cs
@@ -1,11 +1,21 @@
 using System;
 class aula10
 {
-    enum DiasSemana{Domingo, Segunda, Terça,Quarta,Quinta,Sexta,Sábado};
+    enum DiasSemana{Domingo="", Segunda="", Terça="",Quarta="",Quinta="qui",Sexta="sex",Sábado="sáb"};
 
     static void Main()
     {
-    int ds=(int)DiasSemana.Sexta;
-        Console.WriteLine(ds);
+
+    string dia1 = DiasSemana.Domingo;
+    string dia2 = DiasSemana.Segunda;
+    string dia3 = DiasSemana.Terça;
+    string dia4 = DiasSemana.Quarta;
+    string dia5 = DiasSemana.Quinta;
+    string dia6 = DiasSemana.Sexta;
+    string dia7 = DiasSemana.Sábado;
+
+      int Çhocado_Seará = 2;
+    
+        
     }
 }
